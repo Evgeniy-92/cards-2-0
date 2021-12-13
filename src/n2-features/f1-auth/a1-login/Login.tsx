32bbb35d177e47de2cc7db3122ee87f709537539
alex <gromvolk97@gmail.com>
2021-12-13 12:16:15 +0300

    feature(login): create component and added styles

--- src/n2-features/f1-auth/a1-login/Login.tsx
+++ src/n2-features/f1-auth/a1-login/Login.tsx
@@ -1,7 +1,46 @@
+import styles from './styles.module.scss'
+import SuperInputText from "../../../n1-main/m1-ui/common/c2-SuperInputText/SuperInputText";
+import SuperButton from "../../../n1-main/m1-ui/common/c1-SuperButton/SuperButton";
+
 export const Login = () => {
     return (
-        <>
-            <h1>Login</h1>
-        </>
+        <div className={styles.loginMain}>
+            <div className={styles.wrapper}>
+                <div className={styles.login}>
+
+                    <div className={styles.intro}>
+                        <h1>It-incubator</h1>
+                        <h3> Sign In</h3>
+                    </div>
+
+                    <div className={styles.inputs}>
+                        <div className={styles.inputsBox}>
+                            <label>Email</label>
+                            <SuperInputText type={"text"} className={styles.input}
+                            /></div>
+                        <div className={styles.inputsBox}>
+                            <label>Password</label>
+                            <SuperInputText type={'password'} className={styles.input}
+                            /></div>
+                        <div className={styles.forgot}>
+                            <p>
+                                Forgot Password
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.next}>
+                        <SuperButton className={styles.btn}>Login</SuperButton>
+                        <span className={styles.notAccount}>
+                            Don’t have an account?
+                        </span>
+                        <p className={styles.singUp}>
+                            Sing Up
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     )
 }
