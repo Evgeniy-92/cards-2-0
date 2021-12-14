0181a7796f895f03590fa8300451dac164caa1a0
Evgeniy <ev.drozd.2020@gmail.com>
2021-12-15 00:11:22 +0300

    added styles component CheckEmail.tsx

--- src/n2-features/f1-auth/a3-forget/f1-ui/CheckEmail.tsx
+++ src/n2-features/f1-auth/a3-forget/f1-ui/CheckEmail.tsx
@@ -2,6 +2,7 @@ import {useSelector} from "react-redux";
 import {AppRootStateType} from "../../../../n1-main/m2-bll/store";
 import styles from "./CheckEmail.module.scss";
 import ContainerAuth from "../../../../n1-main/m1-ui/common/c4-containerAuth";
+import iconEmail from '../f4-image/icon.png'
 
 export const CheckEmail = () => {
     const email = useSelector<AppRootStateType, string>(state => state.recovery.email)
@@ -10,6 +11,9 @@ export const CheckEmail = () => {
             <div className={styles.container}>
                 <div className={styles.intro}>
                     <h1>It-incubator</h1>
+                    <div className={styles.iconBox}>
+                        <img className={styles.icon} src={iconEmail}/>
+                    </div>
                     <h3>Check Email</h3>
                     <div className={styles.message}>{`We sent an Email with instructions to ${email}, you can close this tab`}</div>
                 </div>
